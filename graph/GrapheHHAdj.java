14a9d98cf19c9606ea48121ae89fa5bc65fa431f
yacoub <157528738+yyy219@users.noreply.github.com>
2025-05-18 23:26:34 +0200

    Update GrapheHHAdj.java
    commentaire graphehhadj

--- graph/GrapheHHAdj.java
+++ graph/GrapheHHAdj.java
@@ -7,21 +7,24 @@ import java.util.Map;
 
 public class GrapheHHAdj implements VarGraph {
 
-	// Représentation du graphe : un sommet -> liste de ses arcs sortants
+	// Représentation du graphe avec une hasmap : un sommet -> liste de ses arcs sortants
 	private Map<String, List<Arc<String>>> adjacence;
 
 	public GrapheHHAdj() {
+		// On crée une hashmap vide
 		this.adjacence = new HashMap<>();
 	}
 
 	@Override
 	public List<Arc<String>> getSucc(String s) {
+		// On retourne la liste des successeurs si le sommet existe, sinon on retourne une liste vide
 		return adjacence.getOrDefault(s,new ArrayList<>());
 	}
 
 	@Override
 	public void ajouterSommet(String noeud) {
 		if (!adjacence.containsKey(noeud)) {
+			// Si le sommet n'esxite pas deja on le met dans une liste
 			adjacence.put(noeud, new ArrayList<Arc<String>>());
 		}
 	}
@@ -33,6 +36,7 @@ public class GrapheHHAdj implements VarGraph {
 			ajouterSommet(source);
 		if (!adjacence.containsKey(destination))
 			ajouterSommet(destination);
+		//on ajoute la destination à la liste des successeurs de la source
 		adjacence.get(source).add(new Arc<>(valeur,destination));
 		
 	}
